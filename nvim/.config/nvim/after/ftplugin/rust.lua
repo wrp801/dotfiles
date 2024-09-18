e78cd2c1e51c6eab20c81b721814071b6b138ccf
wrp801 <wespoulsen@gmail.com>
2024-09-17 19:53:33 -0600

    removed bufferline tabs

--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -1,1 +1,18 @@
 vim.opt_local.formatoptions:remove "o"
+local set = vim.opt_local
+local keymap = vim.keymap
+local builtin = require('telescope.builtin')
+
+set.expandtab = true
+set.tabstop = 4
+set.shiftwidth = 4
+
+local function show_diagnostics() 
+  local opts = {
+    bufnr = 0, 
+    severity = { min = vim.diagnostic.severity.WARN}
+  }
+  builtin.diagnostics(opts)
+end
+keymap.set('n','<leader>db', ':lua show_diagnostics()<CR>', { noremap = true, silent = true})
+-- filter 
