606ba4dc62be9140a6c995fdc59bea2fa535ccdd
wrp801 <wespoulsen@gmail.com>
2024-11-25 19:51:35 -0700

    added fix for tmux on windows terminal

--- nvim/.config/nvim/lua/wrp/core/opts.lua
+++ nvim/.config/nvim/lua/wrp/core/opts.lua
@@ -36,6 +36,7 @@ vim.opt.backspace = "indent,eol,start"
 
 -- system clipboard 
 vim.opt.clipboard:append("unnamedplus")
+vim.g.clipboard = { name = "WslClipboard", copy = { ["+"] = "clip.exe", ["*"] = "clip.exe", }, paste = { ["+"] = [[powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]], ["*"] = [[powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]], }, cache_enabled = 0 }
 
 -- aesthetics
 vim.opt.background = "dark"
