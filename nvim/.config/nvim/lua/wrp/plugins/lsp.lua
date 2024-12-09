ce0dc6a9e50fbc7fa3db1b015bac6ed449293655
wrp801 <wespoulsen@gmail.com>
2024-12-08 20:58:10 -0700

    "reverting lsp changes"

--- nvim/.config/nvim/lua/wrp/plugins/lsp.lua
+++ nvim/.config/nvim/lua/wrp/plugins/lsp.lua
@@ -1,24 +1,18 @@
 return {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
-
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
-
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }   
-
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      'folke/lazydev.nvim',
+      ft = 'lua', -- only load lua files
+      opts = {
+        library = {
+          { path = "${3rd}/luv/library", words = {"vim%.uv"} },
+        },
+      },
+    },
+    config = function()
+      require("lspconfig").lua_ls.setup {}
+    end,
+  }
 }
 
