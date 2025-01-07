781c6439566cddc03a84c33702b809144a032572
wrp801 <wespoulsen@gmail.com>
2025-01-06 20:17:41 -0700

    modified setup script

--- setup.sh
+++ setup.sh
@@ -13,98 +13,91 @@
 # 9. Node and NVM
 # 10. Julia
 
-
+# Install Tmux and TPM
 install_tmux_and_tpm() {
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    if ! command -v tmux &>/dev/null; then
+        sudo apt update && sudo apt install -y tmux
+    fi
+    if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    else
+        echo "TPM is already installed"
+    fi
 }
 
-
+# Check and install function
 check_install() {
-    if command -v $1 &> /dev/null; then
+    if command -v "$1" &>/dev/null; then
         echo "$1 is already installed"
     else
         echo "Installing $1..."
         $2
-    fi  
-}
-
-
-# neovim
-install_neovim() {
-    curl "-LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
-    sudo rm -rf /opt/nvim
-    sudo tar -C /opt -xzf nvim-linux64.tar.gz
-
-    # if command -v nvim >/dev/null 2>&1; then
-    #     echo "Neovim is already installed"
-    # else
-    #     curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-    #     sudo rm -rf /opt/nvim
-    #     sudo tar -C /opt -xzf nvim-linux64.tar.gz
-    # fi
-}
-
-# rust
-install_rust() {
-    "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
-    source ~/.cargo/env/bin
-    # if command -v cargo >/dev/null 2>&1; then 
-    #     echo "Rust is already installed"
-    # else
-    #     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-    #     source ~/.cargo/env/bin
-    # fi
-}
-
-
-# go
-install_go() {
-    if command -v go >/dev/null 2>&1; then 
-        echo "Go is already installed"
-    else
-         rm -rf /usr/local/go && tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
-         # echo "export PATH=$PATH:/usr/local/go/bin" >> ./zsh/.zshrc
     fi
 }
 
-# nix 
+# Neovim
+install_neovim() {
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+    sudo rm -rf /opt/nvim
+    sudo tar -C /opt -xzf nvim-linux64.tar.gz
+    sudo ln -sf /opt/nvim/bin/nvim /usr/local/bin/nvim
+    rm nvim-linux64.tar.gz
+}
+
+# Rust
+install_rust() {
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source "$HOME/.cargo/env"
+}
+
+# Go
+install_go() {
+    curl -LO https://golang.org/dl/go1.23.4.linux-amd64.tar.gz
+    sudo rm -rf /usr/local/go
+    sudo tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
+    echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+    source ~/.bashrc
+    rm go1.23.4.linux-amd64.tar.gz
+}
+
+# Nix
 install_nix() {
-    sudo install -d -m755 -o $(id -u) -g $(id -g) /nix
     curl -L https://nixos.org/nix/install | sh
-    source $HOME/.nix-profile/etc/profile.d/nix.sh
+    . "$HOME/.nix-profile/etc/profile.d/nix.sh"
 }
 
-# zsh 
+# Zsh
 install_zsh() {
-    sudo apt-get install zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" # download ohmyzsh
-    chsh -s $(which zsh) # make zsh the default
+    sudo apt update && sudo apt install -y zsh
+    if [ ! -d "$HOME/.oh-my-zsh" ]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    fi
+    chsh -s "$(which zsh)"
 }
 
-# node and nvm 
+# Node and NVM
 install_node() {
-    sudo apt install nodejs
-    sudo apt install npm
+    sudo apt update && sudo apt install -y nodejs npm
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 }
 
-# julia
+# Julia
 install_julia() {
     curl -fsSL https://install.julialang.org | sh
 }
 
-# ripgrep 
+# Ripgrep
 install_ripgrep() {
-    sudo apt-get install ripgrep
+    sudo apt update && sudo apt install -y ripgrep
 }
 
-# gnu stow 
+# GNU Stow
 install_stow() {
-    apt install -y stow
+    sudo apt update && sudo apt install -y stow
 }
 
-## Installation 
-
+# Installation
+sudo apt update
 check_install nvim install_neovim
 check_install cargo install_rust
 check_install go install_go
@@ -112,7 +105,7 @@ check_install tmux install_tmux_and_tpm
 check_install zsh install_zsh
 check_install node install_node
 check_install julia install_julia
-check_install ripgrep install_ripgrep
-check_install nix install_nix
+check_install rg install_ripgrep
 check_install stow install_stow
+check_install nix install_nix
 
